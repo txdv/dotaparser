bf4b3fb8f84cab4fc535c97b9797de76abc5f458
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-08-21 00:04:39 +0300

    Add another example.

---
+++ examples/drop.js
@@ -0,0 +1,33 @@
+var dotaparser = require('../main'),
+    timespan = require('timespan'),
+    ansi = require('ansi'),
+    cursor = ansi(process.stdout),
+    print = process.stdout.write,
+    fs = require('fs');
+
+var itemlist = { };
+var items = dotaparser.data.ItemList.Item;
+for (var i = 0; i < items.length; i++) {
+  var item = items[i];
+  itemlist[item.Id] = item;
+}
+
+dotaparser.replay3(process.argv[2], function (game, event) {
+  if (event === undefined) {
+    return;
+  }
+  switch (event.type) {
+  //case 'dropitem':
+  //case 'selection':
+  case 'unitbuilding':
+    var item = itemlist[event.itemid];
+    if (item === undefined) {
+      return;
+    }
+    if (item.Type == 'ITEM') {
+      console.log(event.player);
+      console.log(item);
+    }
+    break;
+  }
+});
